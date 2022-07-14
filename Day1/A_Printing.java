99ae583e14731bb040aab805bcff05c8985641c0
proRamLOGO <00shubhbansal@gmail.com>
2022-07-14 22:37:38 +0530

    D1&D2

---
+++ Day1/A_Printing.java
@@ -0,0 +1,7 @@
+public class A_Printing {
+	public static void main(String[] args) {
+		
+		System.out.println( "SHUBH" ); 
+		
+	}
+}
---
+++ Day1/E_ArithmeticOperations.java
@@ -0,0 +1,29 @@
+public class E_ArithmeticOperations {
+	public static void main(String[] args) {
+		System.out.println("6/3");
+		System.out.println(6/3);
+		System.out.println(3+5);
+		System.out.println(6-3);
+		System.out.println(6*3);
+		System.out.println(3/5);
+		System.out.println(5/2);
+		System.out.println(6/3+4);
+		System.out.println(5/3.0);
+		System.out.println(2.5/5);
+		System.out.println(5/2.5);
+		System.out.println(5/3);
+		
+		
+		System.out.println(85%15);
+		System.out.println(69%6);
+		System.out.println(1000%99);
+		System.out.println(35%20);
+		System.out.println(10%2);
+		System.out.println(81%9);
+		System.out.println(100025%25);
+		System.out.println(2%10);
+		System.out.println(5%9);
+		System.out.println(3%16);
+		
+	}
+}
